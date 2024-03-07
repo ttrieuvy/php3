26cd72fdf3a020654fbb0502ad89d8e1a71d773f
ttrieuvy <ttrieuvy2010@gmail.com>
2024-03-07 10:42:04 +0700

    Môn dự án php 3

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Đây là trang chủ</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Trang chủ nè</h1>
+    <h1>Trang tin tức nha</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('trang-chu', function () {
     return view('trangchu');
 });
-Route::get('about', function(){
+
+Route::get('/about', function(){
     return view('about');
+});
+
+Route::get('/news', function(){
+    return view('news');
 });
